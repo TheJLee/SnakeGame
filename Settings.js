32ce5e6dc05205202810f81b6b208cd4909fbf51
Lee <Justin.Lee@experian.com>
2017-09-21 09:26:45 -0700

    first commit

---
+++ Settings.js
@@ -0,0 +1,19 @@
+/*
+********************************************************************************
+So what I need to do is:
+  Makes a Snake
+    Make this an array with each element containg the location of a block
+      If it grows pop off the end and add to the head with the unshift()
+      funciton.
+  Make Food
+    Needs to be random
+  Collision
+    Check to see if head of the snake touches the canvas or itself
+  Grow
+    Touches on the food
+      If food.location = head.location then grow() snake
+  Controls
+    Pause
+    Arrow keys {Up, Down, Left, Right}
+********************************************************************************
+*/
